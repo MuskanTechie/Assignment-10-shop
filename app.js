fb444d4c34fbca6599b38a9a444715b51cd7d552
MuskanTechie <syedamuskanfatima512@gmail.com>
2025-04-28 00:04:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+//SHOP
+
+alert("WELCOME MAM/SIR");
+let item=prompt("Enter name of the item you want to buy");
+let price=prompt("Enter price");
+let quantity=prompt("Enter Quantity");
+console.log(`You have purchased ${item} having price of ${price} in ${quantity} quantity and your subtotal is ${price*quantity}`);
